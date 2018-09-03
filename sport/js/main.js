816787b33074f24fe9a6711820eaf8aaa85d841e
arouetta <42380559+arouetta@users.noreply.github.com>
2018-09-03 09:59:25 +0100

    Add files via upload

---
+++ sport/js/main.js
@@ -0,0 +1,42 @@
+$('.burger-button').click(function() {
+
+    $('.nav-left').css('opacity', '1')
+
+    $('.nav-left').css('left', '0')
+
+})
+
+
+
+$('#close').click(function() {
+
+    $('.nav-left').css('opacity', '0')
+
+    $('.nav-left').css('left', '-300px')})
+
+
+$('#day1').click( function(){
+    $('.alert').css('display','block')
+})
+
+$('#day2').click( function(){
+    $('.alert2').css('display','block')
+})
+$('#day3').click( function(){
+    $('.alert3').css('display','block')
+})
+$('#day4').click( function(){
+    $('.alert4').css('display','block')
+})
+$('#day5').click( function(){
+    $('.alert5').css('display','block')
+})
+$('#day6').click( function(){
+    $('.alert6').css('display','block')
+})
+$('#day7').click( function(){
+    $('.alert7').css('display','block')
+})
+$('#mybtn').after("#shadow:active");
+
+
